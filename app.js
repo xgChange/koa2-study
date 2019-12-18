154ca7656bf216ad7b06d910a8ed310076b1a82c
maoxian <maoxianxiang@infinite.net.cn>
2019-12-18 10:30:12 +0800

    feat: 增加ejs配置

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const Koa = require("koa")
 const Router = require("koa-router")
+const views = require("koa-views")
 
 const app = new Koa()
 const router = new Router()
 const PORT = 3000
+
+app.use(
+  views(__dirname + "/views", {
+    extension: "ejs"
+  })
+)
 
 app.use(async (ctx, next) => {
   // 无论app.use放在上面还是下面，都是在路由之前执行
@@ -21,14 +28,14 @@ router
   .get("/", async ctx => {
     // 获取get 传值
     /* 获取get传值  ctx.query,  ctx.queryString, ctx.request.url */
-
-    ctx.body = "这是首页"
-    console.log("首页")
+    await ctx.render("index", {
+      name: "李四"
+    })
   })
-  .get("/newscontent/:newsId", ctx => {
+  .get("/newscontent/:newsId", async ctx => {
     // 动态路由, 如果有多个传值可以写成： /newsId/newsId2  匹配成: {newsId: xx, newsId2: cc}
     // 获取动态路由的传参 ctx.params
-    console.log(ctx.params)
+    ctx.body = ctx.params
   })
 
 app.use(router.routes()).use(router.allowedMethods())
--- note.md
+++ note.md
@@ -1,6 +1,6 @@
-## koa2 笔记
+# koa2 笔记
 
-koa2 中间件执行顺序
+## koa2 中间件执行顺序
 
 ```javascript
 app.use(async (ctx, next) => {
@@ -16,4 +16,24 @@ app.use(async (ctx, next) => {
 })
 
 // 顺序： 中间件1-中间件2-回调2-回调1
+```
+
+## ejs 模板引擎的使用
+
+```javascript
+/*
+  1. npm i koa-views
+  2. npm i ejs
+  3. app.use(views(__dirname, { extension: 'ejs' }))
+  4. await ctx.render('user.pug')
+*/
+
+app.use(
+  views("views", {
+    // 这种方式使用的话，后缀名是html。将html映射为ejs
+    map: {
+      html: "ejs"
+    }
+  })
+)
 ```
